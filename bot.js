08f283d979a8ab32c9a5f6d91da2cddca7bb2562
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-08-15 16:37:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -428,21 +428,7 @@ return;
 
 
 
-client.on('message', message => {
-    if (message.author.bot) return;
-    if (!message.content.startsWith(prefix)) return;
-  
-    let command = message.content.split(" ")[0];
-    command = command.slice(prefix.length);
-  
-    let args = message.content.split(" ").slice(1);
-  
-    if (command == "say") {
-        if(!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.reply("لا يوجد لديك صلاحية");
-        message.replay(args.join("  "))
-        message.delete()
-       }
-});
+
 
 
 
@@ -489,6 +475,23 @@ client.on("guildMemberAdd", member => {
 
 
 
+
+client.on('message', message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+  
+    let command = message.content.split(" ")[0];
+    command = command.slice(prefix.length);
+  
+    let args = message.content.split(" ").slice(1);
+  
+    if (command == "say") {
+        if(!message.member.hasPermission("VIEW_AUDIT_LOG")) return message.reply("لا يوجد لديك صلاحية");
+         message.delete()
+        message.channel.send(`<@${message.author.id}> ${args} `)
+       
+       }
+});
 
 
 
