9647aab36faf8fbd516650d152aa01287d57a097
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-01 17:48:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1386,7 +1386,55 @@ client.on('message', message => {
     }
 });
 
+const bannedwords = [
+  "#credit",
+  "#profile",
+  "#rep",
+  "#top",
+  "%level",
+  "%تقديم",
+  "-play",
+  "-stop",
+  "-p",
+  "-s",
+  "!invites",
+  "!top",
+  "G.play",
+  "G.stop",
+  "G.skip",
+  "-skip"
+  "$play"
+  "!play"
+]
+client.on('message', message => {
+  var Muted = message.guild.roles.find("name", "muted");
+  var warn = message.guild.roles.find("name", "warn");
+  if(bannedwords.some(word => message.content.includes(word))) {
+  if(message.channel.id !== '481475376212606987') return;
+  if (message.author.bot) return;
+  if(message.member.roles.has(warn)) return;
+  if(!message.member.roles.has(warn.id)) {
+  message.member.addRole(warn)
+  message.reply("**`تم اعطائك تحذير لاستخدام اوامر البوت فى الشات العام` 😠**")
+  }
+  if(message.member.roles.has(warn.id)) {
+      message.member.addRole(Muted)
+      message.member.removeRole(warn)
+      message.reply("**`تم اعطائك ميوت كتابى تواصل مع احد اعضاء الادارة لازالتة` 🤐**")
+  }
+  }
+  })
 
+client.on ("guildMemberAdd", member => {
+  
+   var role = member.guild.roles.find ("name", "لاونر");
+   member.addRole (role);
+  
+})
+
+client.on ("guildMemberRemove", member => {
+   
+})
 
 client.login(process.env.BOT_TOKEN);
 
