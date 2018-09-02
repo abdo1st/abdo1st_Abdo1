e103502e4c68cfbf55546e2fc7ddec4dadb916e4
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-02 10:05:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1482,7 +1482,7 @@ const bannedwords = [
 ]
 client.on('message', message => {
   var Muted = message.guild.roles.find("name", "Muted");
-  var warn = message.guild.roles.find("name", "تحذير");
+  var warn = message.guild.roles.find("name", "warn");
   if(bannedwords.some(word => message.content.includes(word))) {
   if(message.channel.id !== '465151555759243264') return;
   if (message.author.bot) return;
