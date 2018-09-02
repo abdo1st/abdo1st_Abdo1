0ca4b5968fe38c4900d20f65fa25a5389ac12c9d
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-02 09:42:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1375,9 +1375,9 @@ client.on('message', message => {
             اوامر الاغاني
 ╚[❖════════════❖]╝
 
-❖ !play ➾ لتشغيل الاغنية 
+❖ $play ➾ لتشغيل الاغنية 
 
-❖ !stop ➾ لإيقاف الاغنية
+❖ $stop ➾ لإيقاف الاغنية
 
 ==================================================================
 
@@ -1397,6 +1397,53 @@ client.on ("guildMemberAdd", member => {
 client.on ("guildMemberRemove", member => {
    
 })
+
+
+
+
+
+
+
+const bannedwords = [
+  "#credit",
+  "#profile",
+  "#rep",
+  "#top",
+  "%level",
+  "%تقديم",
+  "احا",
+  "كلب",	
+  "-play",
+  "-stop",
+  "-p",
+  "-s",
+  "!invites",
+  "!top",
+  "G.play",
+  "G.stop",
+  "G.skip",
+  "-skip"
+
+]
+client.on('message', message => {
+  var Muted = message.guild.roles.find("name", "muted");
+  var warn = message.guild.roles.find("name", "تحذير");
+  if(bannedwords.some(word => message.content.includes(word))) {
+  if(message.channel.id !== '482328459503206431') return;
+  if (message.author.bot) return;
+  if(message.member.roles.has(warn)) return;
+  if(!message.member.roles.has(warn.id)) {
+  message.member.addRole(warn)
+  message.reply("**`تم اعطائك تحذير لاستخدام اوامر البوت فى الشات العام` 😠**")
+  }
+  if(message.member.roles.has(warn.id)) {
+      message.member.addRole(Muted)
+      message.member.removeRole(warn)
+      message.reply("**`تم اعطائك ميوت كتابى تواصل مع احد اعضاء الادارة لازالتة` 🤐**")
+  }
+  }
+  })
+
 
 
 client.login(process.env.BOT_TOKEN);
