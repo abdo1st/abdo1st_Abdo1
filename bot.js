28db5fc7727f8b555e1cf9f49c1306c65d800128
abdo1st <42328037+abdo1st@users.noreply.github.com>
2018-09-09 04:13:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1259,7 +1259,16 @@ client.on('voiceStateUpdate', (oldM, newM) => {
 
 
 
+client.on("message", (message) => {
+    if (message.content.startsWith('$del')) {
+        if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
 
+        let args = message.content.split(' ').slice(1);
+        let channel = message.client.channels.find('name', args.join(' '));
+        if (!channel) return message.reply('**There is no room like this name -_-**').catch(console.error);
+        channel.delete()
+    }
+});
 
 
 
